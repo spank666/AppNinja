c054306602dbe83aba5b2954df5a1f20b053a689
victo <victo@SPANKDEVPC>
2025-12-08 20:25:26 -0600

    Agregar archivos de proyecto.

---
+++ MoneyTransfer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyTransfer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MoneyTransfer/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MoneyTransfer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
